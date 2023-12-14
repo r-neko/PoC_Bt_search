af840a514f887658d0b3794c950cf867d94494dc
ろむねこ <nem@nem.x0.to>
2023-12-14 15:35:41 +0900

    ブロードキャストレシーバー実装

--- app/src/main/java/me/r0m/trident/poc_bt_search/BtBroadcastReceiver.java
+++ app/src/main/java/me/r0m/trident/poc_bt_search/BtBroadcastReceiver.java
@@ -1,4 +1,38 @@
 package me.r0m.trident.poc_bt_search;
 
-public class BtBroadcastReceiver {
+
+import android.app.Application;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+
+public class BtBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(BluetoothDevice.ACTION_FOUND)) {
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            Context applicationContext = MainActivity.getContextOfApplication(); // Get context of application
+            if (ActivityCompat.checkSelfPermission(applicationContext, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            if (device.getName() != null) {
+                Log.d("BT", "Found device: " + device.getName() + " " + device.getAddress());
+            } else {
+                Log.d("BT", "Found device: " + device.getAddress() + "(Unknown name)");
+            }
+        }
+    }
 }
