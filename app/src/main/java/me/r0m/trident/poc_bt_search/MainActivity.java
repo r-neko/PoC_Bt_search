60ace20fbefbd9a384bb4de61790deabc8b6b5dd
ろむねこ <nem@nem.x0.to>
2023-12-14 15:42:08 +0900

    実装

--- app/src/main/java/me/r0m/trident/poc_bt_search/MainActivity.java
+++ app/src/main/java/me/r0m/trident/poc_bt_search/MainActivity.java
@@ -1,14 +1,27 @@
 package me.r0m.trident.poc_bt_search;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import me.r0m.trident.poc_bt_search.BtBroadcastReceiver;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static Context getContextOfApplication() {
+        return getContextOfApplication();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +34,34 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        btBloadcastReceiver = new BtBroadcastReceiver();
 
+        // Init Broadcast Receiver
+
+        BtBroadcastReceiver btBroadcastReceiver = new BtBroadcastReceiver();
+
+
+        // Intent Filter
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(btBroadcastReceiver, intentFilter);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+
+        if (BluetoothAdapter.getDefaultAdapter().isDiscovering()) {
+            // Discovery is already in progress
+        } else {
+            Log.d("BT", "Bluetooth is not discovering. May not be enabled.");
+        }
     }
 }
